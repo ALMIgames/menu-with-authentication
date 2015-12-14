1c29afa51ed2cef15bba0df7abaccd569e31e9e3
albert <albertxerta93@hotmail.com>
2015-12-14 19:56:55 +0100

    get menu started and 'MenuWithAuthentication' => MenuWithAuthentication\MenuWithAuthentication::class,added

--- app/MenuWithAuthentication.php
+++ app/MenuWithAuthentication.php
@@ -14,21 +14,49 @@ use MenuWithAuthentication\Menu\MenuItem;
 class MenuWithAuthentication {
 
 
+    /**
+     * @var null
+     */
     protected static $instance = null;
 
+    /**
+     * @var MenuItem[]
+     */
+    protected $menu;
+
+
+    /**
+     * @param $id
+     * @return MenuItem
+     */
     function __construct()
     {
     }
 
+    /**
+     * @param $id
+     * @return MenuItem
+     */
     public static function menu($id)
     {
         return new MenuItem($id);
+
     }
 
+    /**
+     * @return null|static
+     */
     public static function instance(){
         if(is_null(static::$instance)){
             return static::$instance = new static;
         }
         return static::$instance;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMenu(){
+        return $this->menu->items();
     }
 }
