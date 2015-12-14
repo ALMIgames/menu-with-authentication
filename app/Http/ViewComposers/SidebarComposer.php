3b8756331c32df4106d285fc362245d176c6b302
albert <albertxerta93@hotmail.com>
2015-12-14 17:14:17 +0100

    profile composer started!

---
+++ app/Http/ViewComposers/SidebarComposer.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace MenuWithAuthentication\Http;
+
+/**
+ * Created by PhpStorm.
+ * User: albert
+ * Date: 14/12/15
+ * Time: 17:10
+ */
+
+use View;
+
+/**
+ * Class SidebarComposer
+ * @package MenuWithAuthentication\Http
+ */
+class SidebarComposer {
+    /**
+     * Bind data to the view.
+     *
+     * @param  View  $view
+     * @return void
+     */
+    public function compose(View $view)
+    {
+        
+    }
+}
