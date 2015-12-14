4a848c597acce768f928e39aac81af8017ed4a12
albert <albertxerta93@hotmail.com>
2015-12-14 16:50:38 +0100

    tests with menu.php

--- bootstrap/menu.php
+++ bootstrap/menu.php
@@ -8,15 +8,27 @@
 
 
 //Home
-MenuWithAuthentication::Menu()->title('Home')
+MenuWithAuthentication::Menu()
+    ->title('Home')
+    ->icon('fa-dashboard')
+    ->url('/home')
+    ->rol('home')
+    ->permission('home')
+    ->user('albertmayor');
 
 //Another link
-MenuWithAuthentication::Menu()->title('Another link')
+MenuWithAuthentication::Menu()
+    ->title('Another link')
+    ->icon('fa-link')
+    ->user(1);
 
 //Multilevel
-MenuWithAuthentication::Menu()->title('Multilevel')
-MenuWithAuthentication::Menu()->title('Level2')
-MenuWithAuthentication::Menu()->title('Level3')
+MenuWithAuthentication::Menu()->title('Multilevel')->icon('fa-caret-down');
+MenuWithAuthentication::Menu()->title('Level2')->icon('fa-caret-right');
+MenuWithAuthentication::Menu()->title('Level3')->icon('fa-caret-right');
 
 
-MenuWithAuthentication::Menu()->title('Ultim element')
+MenuWithAuthentication::Menu()
+    ->title('Ultim element')
+    ->icon('fa-ban')
+    ->url('http://www.google.com');
