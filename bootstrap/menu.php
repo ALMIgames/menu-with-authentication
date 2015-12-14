ee14fa4889990f6378a8591358375f95884e35ba
albert <albertxerta93@hotmail.com>
2015-12-14 16:30:18 +0100

    created menu.php

---
+++ bootstrap/menu.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: albert
+ * Date: 14/12/15
+ * Time: 16:28
+ */
+
+
+//Home
+Menu::
+
+//Another link
+Menu::
+
+//Multilevel
+Menu::
+    Menu::
+    Menu::
